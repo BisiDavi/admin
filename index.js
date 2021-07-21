f326b9f6735d90e16b0c91f4300b67e60e25aba7
BisiDavi <oludavidconnect@gmail.com>
2021-07-21 04:43:48 +0100

    main

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ app.set('view engine', '.hbs');
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.render('index', { layout: 'main' });
-});
+function displayView(route, fileName, title) {
+    return app.get(route, function (req, res) {
+        res.render(fileName, { layout: 'main', data: title });
+    });
+}
+
+displayView('/', 'index', 'Dashboard');
+displayView('/active-orders', 'active-orders', 'Active Orders');
+displayView('/create-profile', 'create-profiles', 'Create Profiles');
+displayView('/create-orders', 'create-orders', 'Create Orders');
+displayView('/dispatcher-list', 'dispatcher-list', 'Dispatchers List');
+displayView('/orders-history', 'orders-history', 'Orders History');
+displayView('/orders-status', 'orders-status', 'Orders Status');
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
